afb65efb5190e7532bf55d08068d2af729ac1d90
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-12 11:55:57 +0200

    data

--- src/components/imageSecion/ImageSections.jsx
+++ src/components/imageSecion/ImageSections.jsx
@@ -1,1 +1,36 @@
-export default ImageSections() {}
+const data = [
+  {
+    img: 'https://preview.colorlib.com/theme/personal/img/p1.jpg',
+    title: '2D Vinyl Design',
+    category: 'vector',
+  },
+  {
+    img: 'https://preview.colorlib.com/theme/personal/img/p2.jpg',
+    title: '2D Vinyl Design',
+    category: 'vector',
+  },
+  {
+    img: 'https://preview.colorlib.com/theme/personal/img/p3.jpg',
+    title: 'Creative Poster Design',
+    category: 'Agency',
+  },
+  {
+    img: 'https://preview.colorlib.com/theme/personal/img/p4.jpg',
+    title: 'Embosed Logo Design',
+    category: 'Portal',
+  },
+  {
+    img: 'https://preview.colorlib.com/theme/personal/img/p5.jpg',
+    title: '3D Helmet Design',
+    category: 'vector',
+  },
+  {
+    img: 'https://preview.colorlib.com/theme/personal/img/p6.jpg',
+    title: '2D Vinyl Design',
+    category: 'raster',
+  },
+];
+
+export default function ImageSections() {
+  return <div></div>;
+}
