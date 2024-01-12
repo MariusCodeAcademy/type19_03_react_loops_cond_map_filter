12bea8c29784441f3926b457145a992058ae87f9
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-12 10:44:56 +0200

    list

--- src/App.jsx
+++ src/App.jsx
@@ -7,34 +7,37 @@ function OneLi(props) {
 
 export default function App() {
   const countersDataArr = [
-    { id: 1, title: 'Push ups', value: 500 },
+    { id: 1, title: 'Push ups', value: 500, color: 'bg-warning' },
     { id: 2, title: 'Sit ups', value: 200, color: 'bg-success' },
-    { id: 3, title: 'Squats', value: 300 },
+    { id: 3, title: 'Squats', value: 300, color: 'bg-danger' },
     { id: 4, title: 'Jumping jacks', value: 100 },
   ];
 
+  const listElements = countersDataArr
+    .filter((cObj) => cObj.value > 200)
+    .map((cObj) => (
+      <li key={cObj.id} className='list-group-item'>
+        {cObj.title}
+      </li>
+      // <OneLi key={cObj.id} />
+    ));
+
+  // console.log('listElements ===', listElements);
   return (
     <div className='container'>
       <h1 className='display-2 mt-3'>I am the Vytas App</h1>
 
-      <ul className='list-group my-5  '>
-        {countersDataArr.map((cObj) => (
-          <li key={cObj.id} className='list-group-item'>
-            {cObj.title}
-          </li>
-          // <OneLi key={cObj.id} />
-        ))}
-      </ul>
+      <ul className='list-group my-5'>{listElements}</ul>
 
       <section className='row bg-info'>
-        <div className='col-3 text-center text-white p-4'>
-          <p className='lead fs-1 '>{countersDataArr[0].value}</p>
-          <h3 className='fs3'>{countersDataArr[0].title}</h3>
-        </div>
-        <div className='col-3 text-center text-white p-4'>
-          <p className='lead fs-1 '>{countersDataArr[1].value}</p>
-          <h3 className='fs3'>{countersDataArr[1].title}</h3>
-        </div>
+        {countersDataArr.map((cObj) => (
+          <div
+            key={cObj.id}
+            className={`col-3 text-center text-white p-4 ${cObj.color || ''}`}>
+            <p className='lead fs-1 '>{cObj.value}</p>
+            <h3 className='fs3'>{cObj.title}</h3>
+          </div>
+        ))}
       </section>
     </div>
   );
